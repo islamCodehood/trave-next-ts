0a9300fdc224bc8dc1f79e638e09e675ec049d0d
Islam Sayed <islam.sayed8@gmail.com>
2022-08-25 09:49:24 +0200

    first commit

---
+++ components/layout.tsx
@@ -0,0 +1,19 @@
+import Header from './header'
+import Footer from './footer'
+import React from 'react';
+
+type childrenProps =  {
+  children: React.ReactNode
+}
+
+const Layout = ({children}: childrenProps) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
